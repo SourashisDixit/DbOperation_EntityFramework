975bb783beb6aa489f1c0dea2ae9a0a08822517c
SourashisDixit <dixitsourashis@gmail.com>
2026-08-29 15:25:31 +0530

    Initial EF Core setup

---
+++ DbOperationsWithEFcoreApp/DbOperationsWithEFcoreApp/Controllers/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DbOperationsWithEFcoreApp.Controllers
+{
+    public class AppDbContext:DbContext
+
+    {
+        public AppDbContext(DbContextOptions<AppDbContext>options):base(options)
+        {
+            
+        }
+    }
+}
